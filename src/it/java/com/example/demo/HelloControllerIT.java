921e07ecdc4015d5c31db7046b03fc1936219500
Mahalakshmi Katireddy <mahalkr@amazon.com>
2025-03-23 17:30:34 +0530

    jenkins

--- src/it/java/com/example/demo/HelloControllerIT.java
+++ src/it/java/com/example/demo/HelloControllerIT.java
@@ -12,7 +12,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class ControllerIT {
+public class HelloControllerIT {
 
     @Autowired
     private MockMvc mockMvc;
